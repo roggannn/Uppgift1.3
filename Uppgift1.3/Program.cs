fe071443f87867cce072112b694f5334b689b9a5
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:22:44 +0200

    Add project files.

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uppgift
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(";    /\\");
+           Console.WriteLine(";   /  \\");
+           Console.WriteLine(";  /    \\");
+           Console.WriteLine("; / \"\"   \\");
+           Console.WriteLine(";/________\\");
+        }
+
+    }
+}
